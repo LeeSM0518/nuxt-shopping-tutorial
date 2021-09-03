25d6d4e51944c5d60aff6c1f27c9e8e9d347c12f
Sangmin Lee <20171687@edu.hanbat.ac.kr>
2021-09-03 20:26:35 +0900

    upload

---
+++ pages/cart/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class='container'>
+    <h1 class='list-title'>카트 페이지</h1>
+    <CartList></CartList>
+    <div class='extra-panel'>
+      <button>구매하기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import CartList from '~/components/CartList'
+export default {
+  components: { CartList }
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 2rem 10rem;
+}
+
+.list-title {
+  font-weight: 700;
+  font-size: 1.4rem;
+}
+
+.extra-panel {
+  text-align: right;
+  padding: 0.2rem 0;
+}
+</style>
